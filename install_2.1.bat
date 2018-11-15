f338a87c08a3ccdc452589e8bd0807f9c8dd9947
MouseJW <mouse.jiangwei@qq.com>
2018-11-15 21:57:18 +0800

    创项目
    公司自用安装应用安装脚本

---
+++ install_2.1.bat
@@ -0,0 +1,48 @@
+@ECHO OFF
+adb kill-server
+adb start-server
+setlocal enableDelayedExpansion
+set install_state=0
+:start
+cls
+if "%install_state%"=="0" (
+	echo 等待 usb 设备开始安装软件
+	set install_state=4
+)
+adb devices>devices_serial
+set tra=1
+set serial_1=
+set serial_2=
+set serial_3=
+set serial_4=
+for /f "skip=1 tokens=1 delims=	" %%i in (devices_serial) do (
+	if !tra!==1 (
+		set serial_1=%%i
+		set tra=2
+	) else (
+		if !tra!==2 (
+			set serial_2=%%i
+			set tra=3
+		) else (
+			if !tra!==3 (
+				set serial_3=%%i
+				set tra=4
+			) else (
+				set serial_4=%%i
+				set tra=1
+			) 
+		)
+	)
+)
+echo serial_1:"!serial_1!"
+echo serial_2:"!serial_2!"
+echo serial_3:"!serial_3!"
+echo serial_4:"!serial_4!"
+echo ---------------------------
+start .\install_2.0-core.bat !serial_1!
+start .\install_2.0-core.bat !serial_2!
+start .\install_2.0-core.bat !serial_3!
+start .\install_2.0-core.bat !serial_4!
+echo 当所有设备更换后按任意键开始新一轮升级
+pause
+goto start
