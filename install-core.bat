29db0d3b9e3757f3e918f14b81833963d2106115
MouseJW <mouse.jiangwei@qq.com>
2018-12-06 12:20:49 +0800

    修改 bug、规范代码
    1、修复无法显示当前应用安装数量的 bug
    2、规范变量声明

--- install-core.bat
+++ install-core.bat
@@ -1,18 +1,18 @@
 @echo off
-setlocal enabaleDelayedExpansion
+setlocal enableDelayedExpansion
 rem 支持 UTF-8 字符集
 chcp 65001>nul
 if "%1"=="" exit 
 rem 存储当前脚本路径
-set myPath=%~dp0
+set "myPath=%~dp0"
 rem 保存代码段执行成功与否状态，true 为执行成功，false 为执行失败，默认为 true
-set isSuccess=true
+set "isSuccess=true"
 rem 保存当前代码段位置,应该在进入代码段之后马上设置
 set "nowStep="
 rem 保存最后一次执行出错的代码段的位置
 set "errorStep="
 :start
-set nowStep=start
+set "nowStep=start"
 cls
 echo 设备：%1
 if !isSuccess!==false (
@@ -20,36 +20,34 @@ if !isSuccess!==false (
 	color cf
 	%myPath%\adb.exe -s %1 wait-for-usb-device
 	color 07
-	set isSuccess=true
+	set "isSuccess=true"
 	goto !errorStep!
 )
 
 :install_optional
-set nowStep=install_optional
+set "nowStep=install_optional"
 echo ------------------------------------------------------------
 rem echo 准备安装可选应用
-echo 准备安装应用
+rem echo 准备安装应用
 cd .\app\optional
 if not exist *.apk (
 	echo 未在 app\optional 目录学发现应用,跳过安装
 	echo 注：不能使用中文名，且名字中不含空格，错误的文件名会使脚本出错
-	cd %myPath%
-	color 2f
-	@echo 
-	@echo 
-	@echo 
-	@echo 
+	@echo.
+	@echo.
+	@echo.
+	@echo.
 	@echo 按任意键退出安装
 	pause >nul
-	exit
+	goto exit
 )
-set i_o_tra=0
+set "i_o_tra=0"
 for %%t in (*.apk) do (
-	set /a i_o_tra= !i_o_tra! + 1
+	set /a "i_o_tra= !i_o_tra! + 1"
 	echo 正在安装第 !i_o_tra! 个应用： %%t
 	%myPath%\adb.exe -s %1 install -r %%t
 	if errorlevel 1 (
-		set errorStep=!nowStep!
+		set "errorStep=!nowStep!"
 		goto failed
 	)
 	echo %%t 安装完成
@@ -61,8 +59,8 @@ echo 完成可选应用安装
 goto exit
 
 :failed
-set nowStep=failed
-set isSuccess=false
+set "nowStep=failed"
+set "isSuccess=false"
 cls
 color 4f
 echo 连接设备 %1 异常，按任意键重新开始
