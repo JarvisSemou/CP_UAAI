0ff444868c22332d67c40b2b6675bf84ec50059e
MouseJW <mouse.jiangwei@qq.com>
2019-03-09 03:17:48 +0800

    LogUtil 库 bug 修正
    修正 LogUtil 在65001 编码格式下日期显示错误以及日志文件名与预期不一致的问题

--- core/lib/LogUtil.bat
+++ core/lib/LogUtil.bat
@@ -1,4 +1,9 @@
+@echo off
 rem Save log to the log file and name the log file with date
+
+rem setlocal enableDelayedExpansion
+rem 支持 UTF-8 字符集
+chcp 65001 1>nul
 
 if "%2%"=="log" goto log
 goto eof
@@ -9,7 +14,12 @@ rem return void
 rem parm_3 bat file name,use to explanation whitch bat file print the log
 rem parm_4 log content
 :log
-echo "%time% %3: %~4">>%logPath%%date:~0,4%-%date:~5,2%-%date:~8,2%.log
+rem set dateTmp=""
+rem for /f "tokens=2" %%t in ("%date%") do set dateTmp=%%t
+rem set logFileName=%logPath%!dateTmp:~0,4!-!dateTmp:~5,2!-!dateTmp:~8,2!.log
+set logFileName=%logPath%%date:~3,4%-%date:~8,2%-%date:~11,2%.log
+set logContent="%time% %3: %~4"
+echo %logContent:~1,-1%>>%logFileName%
 goto eof
 
 :eof
